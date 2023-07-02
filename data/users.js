b43033d59aa20e8521157039b65fbed271c08e2a
Yousaf-R878 <yousaf.rajput878@gmail.com>
2023-07-01 22:13:12 -0400

    working on user data funcs

--- data/users.js
+++ data/users.js
@@ -1,13 +1,31 @@
 import { users } from "../config/mongoCollections";
+import * as validation from "../validation.js"
 
 const usersCollection = await users();
 
 const getAllUsers = async () => {
+    const users = await usersCollection.find({}).toArray();
+    return users;
+}
+
+const getUser = async (id) => {
+    const id = validation.checkId(id, "userID");
+    const user = await usersCollection.findOne({ _id: id });
+    return user;
+}
+
+const createUser = async (username, email, password) => {
 
 }
 
+const updateUser = async (id, username, email, password) => {
+
+}
 const exportedMethods = {
-    getAllUsers
+    getAllUsers,
+    getUser,
+    createUser,
+    updateUser
 }
 
 export default exportedMethods;
---
+++ validation.js
@@ -0,0 +1,11 @@
+export function checkId(id, type = "Object", funcName = "Unknown function") {
+    if (!id || typeof id !== "string") {
+        throw `Error in ${funcName}: ${type} ID is required and must be a string`;
+    }
+    if (id.length === 0)
+        throw `Error: ${varName} cannot be an empty string or just spaces`;
+    if (!ObjectId.isValid(id)) {
+        throw `Error in ${funcName}: ${type} ID invalid object ID`;
+    }
+    return id.trim();
+}
